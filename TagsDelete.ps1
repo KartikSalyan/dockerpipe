4b131e74f5249653775acf195df497a654879dae
KartikSalyan <74146425+KartikSalyan@users.noreply.github.com>
2023-03-10 12:15:01 +0530

    Create TagsDelete.ps1

---
+++ TagsDelete.ps1
@@ -0,0 +1,15 @@
+# Set the ACR name and maximum number of tags to keep
+$acrName = "practiceregistryy"
+$acrRepository = "practiceregistryy/practiceregistryy"
+$maxTags = 1
+
+sudo az acr login --name $acrName
+#Get the number of tags
+$tagsCount = (az acr repository show-tags --name $acrName --repository $acrRepository).length-2
+
+if ($tagsCount -gt $maxTags){
+	az acr repository show-tags -n $acrName --repository $acrRepository --orderby time_asc --top 1 --query "[0]" --output tsv | 
+	foreach { 
+		az acr repository delete --name $acrName --image ${acrRepository}:$_ --yes 
+	}
+}
